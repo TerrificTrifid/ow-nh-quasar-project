b870a0d410e16df713e0420e73715a47423754af
TerrificTrifid <99054745+TerrificTrifid@users.noreply.github.com>
2023-07-03 23:35:44 -0500

    Setup and tests

--- QuasarProject/QuasarProject.cs
+++ QuasarProject/QuasarProject.cs
@@ -26,6 +26,13 @@ namespace QuasarProject
             {
                 if (loadScene != OWScene.SolarSystem) return;
                 ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+
+                ModHelper.Events.Unity.FireOnNextUpdate(() => {
+
+                    // fix dumb ship gravity
+                    Locator.GetShipTransform().Find("Volumes/ShipGravityVolume").GetComponent<ShipDirectionalForceVolume>()._insideSpeedLimiter = true;
+
+                });
             };
         }
     }
