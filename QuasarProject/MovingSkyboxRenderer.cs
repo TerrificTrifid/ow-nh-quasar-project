ff8087136fea82998ac23be3e40e475c2942746a
JohnCorby <williamforgames6@gmail.com>
2023-07-10 01:21:54 -0700

    even more untested blah

--- QuasarProject/MovingSkyboxRenderer.cs
+++ QuasarProject/MovingSkyboxRenderer.cs
@@ -3,21 +3,24 @@ using UnityEngine;
 
 namespace QuasarProject;
 
+/// <summary>
+/// parent is scaled to fake increase of far plane
+/// should be draw as SkyboxRenderer so it renders behind everything else (does not do lighting properly)
+///
+/// the idea is to be at the same position as the camera, by scaled by some amount to appear larger
+///
+/// oh yeah this completely breaks with the probe LOL
+/// 
+/// first parent is scaled
+/// second parent is origin
+/// </summary>
 [UsedInUnityProject]
 public class MovingSkyboxRenderer : MonoBehaviour
 {
-    private Transform _origin;
-
-    private void Start()
-    {
-        _origin = QuasarProject.Instance.NewHorizons.GetPlanet("Ring").transform;
-    }
-
     private void Update()
     {
-        // parent is scaled to fake increase of far plane
-        // should be draw as SkyboxRenderer so it renders behind everything else (does not do lighting properly)
-        var playerPos = _origin.InverseTransformPoint(Locator.GetPlayerTransform().position);
+        var origin = transform.parent.parent;
+        var playerPos = origin.InverseTransformPoint(Locator.GetPlayerTransform().position);
         transform.position = playerPos;
     }
 }
