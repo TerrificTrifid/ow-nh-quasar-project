90f011f510b3a4c7a65662587bd3bb4146c4dfc7
JohnCorby <williamforgames6@gmail.com>
2023-07-16 08:36:11 -0700

    actionation delay guy

---
+++ QuasarProject/ActivationDelay.cs
@@ -0,0 +1,30 @@
+﻿using NewHorizons;
+using NewHorizons.Utility.OWML;
+using UnityEngine;
+
+namespace QuasarProject;
+
+// bad
+[UsedInUnityProject]
+public class ActivationDelay : MonoBehaviour
+{
+    public OWTriggerVolume Volume;
+    public GameObject Object;
+    public int Seconds;
+
+    public void Awake()
+    {
+        Volume.OnEntry += OnEntry;
+    }
+
+    public void OnDestroy()
+    {
+        Volume.OnEntry -= OnEntry;
+    }
+
+    private void OnEntry(GameObject hitobj)
+    {
+        if (hitobj.GetAttachedOWRigidbody().CompareTag("Player"))
+            Delay.FireInNUpdates(() => Object.SetActive(true), Seconds * 60);
+    }
+}
