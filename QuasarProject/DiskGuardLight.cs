a8909a83a3372d8701cd8456b57b48aaf5a05011
JohnCorby <williamforgames6@gmail.com>
2023-08-05 18:15:52 -0700

    disk guard light

--- QuasarProject/DiskGuardLight.cs
+++ QuasarProject/DiskGuardLight.cs
@@ -7,12 +7,42 @@ namespace QuasarProject;
 public class DiskGuardLight : MonoBehaviour
 {
 	private Color _lightColor;
-	private float _lightIntesity;
+	private float _lightIntensity;
 	public Color DarkColor;
 	public float DarkIntensity;
 
 	private Light _light;
-	private MeshRenderer _fogRenderer;
+	private Renderer _fogRenderer;
 
-	private void Awake() { }
+	private void Awake()
+	{
+		_light = GetComponent<Light>();
+		_fogRenderer = GetComponent<Renderer>();
+
+		if (_light)
+		{
+			_lightColor = _light.color;
+			_lightIntensity = _light.intensity;
+		}
+		else if (_fogRenderer)
+		{
+			_lightColor = _fogRenderer.material.color;
+		}
+	}
+
+	public void UpdateValues(float t)
+	{
+		var color = Color.Lerp(_lightColor, DarkColor, t);
+		var intensity = Mathf.Lerp(_lightIntensity, DarkIntensity, t);
+
+		if (_light)
+		{
+			_light.color = color;
+			_light.intensity = intensity;
+		}
+		else if (_fogRenderer)
+		{
+			_fogRenderer.material.color = color;
+		}
+	}
 }
