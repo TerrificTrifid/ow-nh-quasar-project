77cc19af42b669cee6bbfa0e7543e2093db84cf7
JohnCorby <williamforgames6@gmail.com>
2023-08-05 18:24:35 -0700

    disk guard pass controller

--- QuasarProject/DiskGuardPassController.cs
+++ QuasarProject/DiskGuardPassController.cs
@@ -8,4 +8,28 @@ public class DiskGuardPassController : MonoBehaviour
 {
 	public Transform[] Points;
 	public AnimationCurve DistanceCurve;
+
+	private DiskGuardLight[] _lights;
+
+	private void Awake()
+	{
+		_lights = FindObjectsOfType<DiskGuardLight>();
+	}
+
+	private void Update()
+	{
+		foreach (var light in _lights)
+		{
+			var minDistance = float.PositiveInfinity;
+
+			foreach (var point in Points)
+			{
+				var distance = Vector3.Distance(light.transform.position, point.position);
+				if (distance < minDistance) minDistance = distance;
+			}
+
+			var t = DistanceCurve.Evaluate(minDistance);
+			light.UpdateValues(t);
+		}
+	}
 }
