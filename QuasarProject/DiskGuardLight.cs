9becb2f27e6dbaff1247b057e3c1131849a58091
JohnCorby <williamforgames6@gmail.com>
2023-08-05 18:04:12 -0700

    outline for disk guard

---
+++ QuasarProject/DiskGuardLight.cs
@@ -0,0 +1,18 @@
+﻿using NewHorizons;
+using UnityEngine;
+
+namespace QuasarProject;
+
+[UsedInUnityProject]
+public class DiskGuardLight : MonoBehaviour
+{
+	private Color _lightColor;
+	private float _lightIntesity;
+	public Color DarkColor;
+	public float DarkIntensity;
+
+	private Light _light;
+	private MeshRenderer _fogRenderer;
+
+	private void Awake() { }
+}
---
+++ QuasarProject/DiskGuardPassController.cs
@@ -0,0 +1,11 @@
+﻿using NewHorizons;
+using UnityEngine;
+
+namespace QuasarProject;
+
+[UsedInUnityProject]
+public class DiskGuardPassController : MonoBehaviour
+{
+	public Transform[] Points;
+	public AnimationCurve DistanceCurve;
+}
