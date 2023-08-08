4c512e35f236f389c1de7dd4ba0c1d29ef51869d
JohnCorby <williamforgames6@gmail.com>
2023-08-08 16:26:07 -0700

    DiskGuardPassEffect: support audio

--- QuasarProject/DiskGuardPassEffect.cs
+++ QuasarProject/DiskGuardPassEffect.cs
@@ -8,28 +8,36 @@ public class DiskGuardPassEffect : MonoBehaviour
 {
 	private Color _defaultColor;
 	private float _defaultIntensity;
+	private float _defaultVolume;
 	public Color PassColor;
 	public float PassIntensity;
 	public Light PassAmbientLight;
+	public float PassVolume;
 
 	private Light _light;
 	private Renderer _renderer;
+	private AudioSource _audioSource;
 	private bool _isDefault;
 
 	private void Awake()
 	{
 		_light = GetComponent<Light>();
 		_renderer = GetComponent<Renderer>();
+		_audioSource = GetComponent<AudioSource>();
 		_isDefault = true;
 
 		if (_light)
 		{
 			_defaultColor = _light.color;
 			_defaultIntensity = _light.intensity;
-        }
+		}
 		else if (_renderer)
 		{
 			_defaultColor = _renderer.material.color;
+		}
+		else if (_audioSource)
+		{
+			_defaultVolume = _audioSource.volume;
 		}
 	}
 
@@ -48,19 +56,23 @@ public class DiskGuardPassEffect : MonoBehaviour
 		if (_light)
 		{
 			_light.intensity = Mathf.Lerp(_defaultIntensity, PassIntensity, t);
-            if (PassAmbientLight)
+			if (PassAmbientLight)
 			{
 				var t2 = Mathf.Lerp(t, 1 - t, 1 - t);
 				PassAmbientLight.intensity = Mathf.Lerp(_defaultIntensity, PassIntensity, t2);
-            }
+			}
 			else
 			{
-                _light.color = Color.Lerp(_defaultColor, PassColor, t);
-            }
+				_light.color = Color.Lerp(_defaultColor, PassColor, t);
+			}
 		}
 		else if (_renderer)
 		{
 			_renderer.material.color = Color.Lerp(_defaultColor, PassColor, t);
-        }
+		}
+		else if (_audioSource)
+		{
+			_audioSource.volume = Mathf.Lerp(_defaultVolume, PassVolume, t);
+		}
 	}
 }
