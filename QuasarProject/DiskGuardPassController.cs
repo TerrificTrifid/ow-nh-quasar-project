e9f04d126cfdbb2786e0cc636bff0261ca4a7b7d
JohnCorby <williamforgames6@gmail.com>
2023-08-08 19:07:23 -0700

    shadow hack

--- QuasarProject/DiskGuardPassController.cs
+++ QuasarProject/DiskGuardPassController.cs
@@ -1,6 +1,8 @@
 ﻿using NewHorizons;
 using NewHorizons.Utility.OWML;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace QuasarProject;
 
@@ -11,17 +13,24 @@ public class DiskGuardPassController : MonoBehaviour
 	public AnimationCurve DistanceCurve;
 
 	private DiskGuardPassEffect[] _effects;
+	// for disabling shadows behind sun
+	private Renderer[] _shadowRenderers;
 
 	// do in start to wait for other objects to be built
 	private void Start()
 	{
-		enabled = false;
 		// delay to get the player one
+		enabled = false;
 		Delay.FireOnNextUpdate(() =>
 		{
 			enabled = true;
 			_effects = FindObjectsOfType<DiskGuardPassEffect>();
 		});
+
+		_shadowRenderers = Points
+			.SelectMany(x => x.GetComponentsInChildren<Renderer>(true))
+			.Where(x => x.shadowCastingMode == ShadowCastingMode.On)
+			.ToArray();
 	}
 
 	private void Update()
@@ -38,6 +47,14 @@ public class DiskGuardPassController : MonoBehaviour
 
 			var t = DistanceCurve.Evaluate(minDistance);
 			effect.UpdateValues(t);
+		}
+
+		var starPos = transform.root.position;
+		var toPlayer = Locator.GetPlayerTransform().position - starPos;
+		foreach (var renderer in _shadowRenderers)
+		{
+			var behind = Vector3.Dot(renderer.transform.position - starPos, toPlayer) < 0;
+			renderer.shadowCastingMode = behind ? ShadowCastingMode.Off : ShadowCastingMode.On;
 		}
 	}
 }
